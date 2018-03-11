3e7da6e431aaf2c52bbb6b75b811a77ec3a6ba66
Josh Sample <joshuawsample@gmail.com>
2018-03-11 19:57:56 -0400

    Changed spinning logo for giggles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src='https://images-na.ssl-images-amazon.com/images/I/41-9utO4EZL._SY355_.jpg' className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
